896cd62a36e8b74e0bfb1986a75ca26d50de63f1
Shivam Sharma <sharma.shivam09@gmail.com>
2023-01-10 17:27:58 +0100

    - Colored Lights Hallucinations added

---
+++ Assets/_GameAssets/Scripts/Symptoms/Hallucinations/LightFlaresHallucinations.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.sharmas4.MentalHealthDisorder
+{
+
+    public class LightFlaresHallucinations : MonoBehaviour, ISymptoms
+    {
+        public bool IsSimulating => throw new System.NotImplementedException();
+
+        public void Simulate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
